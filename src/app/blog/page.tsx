fd3ccf3f759cba4ee67c55890f22317efb8babcf
Milo Knowles <miloknowles97@gmail.com>
2024-11-09 11:24:27 -0500

    WIP

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -10,20 +10,19 @@ export const metadata = {
 export default function Page() {
   return (
     <section className="pt-8 pb-8">
-      <h1 className="font-semibold text-3xl mb-8 tracking-tighter">Blog Posts</h1>
+      <h1 className="font-semibold text-3xl mb-8 tracking-tighter">Writing</h1>
       <BlogPostsIndex/>
 
       <Separator className="w-full mt-6 mb-6"/>
 
-      <h1 className="font-semibold text-3xl mb-4 tracking-tighter">Miscellaneous</h1>
       <div className="flex flex-col gap-4">
-        <ExternalLink href="https://medium.com/singularity-energy/what-is-consumed-carbon-intensity-and-why-is-it-important-3e272ee3e08c">
+        <ExternalLink className="text-neutral-800" href="https://medium.com/singularity-energy/what-is-consumed-carbon-intensity-and-why-is-it-important-3e272ee3e08c">
           [Article] What is consumed carbon intensity and why is it important?
         </ExternalLink>
-        <ExternalLink href="/knowles_icra_2021.pdf">
+        <ExternalLink className="text-neutral-800" href="/knowles_icra_2021.pdf">
           [Paper] Toward Robust and Efficient Online Adaptation for Deep Stereo Depth Estimation (ICRA 2021)
         </ExternalLink>
-        <ExternalLink href="https://dspace.mit.edu/handle/1721.1/129167">
+        <ExternalLink className="text-neutral-800" href="https://dspace.mit.edu/handle/1721.1/129167">
           [Thesis] Toward robust deep stereo networks: uncertainty learning, novelty detection, and online adaptation
         </ExternalLink>
       </div>
