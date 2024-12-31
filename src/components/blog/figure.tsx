cc7eaf779f6ba0a57d8f27058fdadde182f42aaa
Milo Knowles <miloknowles97@gmail.com>
2024-12-31 14:50:27 -0500

    Add the idea compass posts (#5)
    * WIP
    
    * Gimmicks
    
    * post 3
    
    * Conclusions
    
    * WIP
    
    * Ack

--- src/components/blog/figure.tsx
+++ src/components/blog/figure.tsx
@@ -1,16 +1,20 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+
 export default function Figure({
   src,
   alt,
   caption,
+  className,
 }: {
   src: string;
   alt: string;
   caption?: string | React.ReactNode;
+  className?: string;
 }) {
   return (
-    <figure className="flex flex-col items-center">
+    <figure className={cn("flex flex-col items-center", className)}>
       <img src={src} alt={alt} className="w-full rounded-lg" />
       {caption && (
         <figcaption className="text-sm text-gray-600 dark:text-gray-400 mt-2">
