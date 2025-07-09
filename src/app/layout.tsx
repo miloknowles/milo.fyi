8951cf8057e64f1f75e9f974690d8fc2e0ace338
Milo Knowles <miloknowles97@gmail.com>
2025-07-09 19:29:46 -0400

    Get rid of navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({
         >
           <Theme radius="medium" grayColor="auto" accentColor="indigo">
             <main className="flex-auto min-w-0 flex flex-col px-2 md:px-0 pt-6">
-              <Navigation />
+              {/* <Navigation /> */}
               {children}
               <Footer />
               <Analytics />
